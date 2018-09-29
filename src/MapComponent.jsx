b97da0ac406a507c701cfa6d031c9be56e596ec5
hcwoo2 <hcwoo2@illinois.edu>
2018-09-29 15:27:21 -0500

    Init google maps component, Henry's API Key

---
+++ src/MapComponent.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
+  }
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  render() {
+    if (!this.props.google) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <Map
+          style={{
+            minWwidth: "200px",
+            minHeight: "200px"
+          }}
+          google={this.props.google}
+          zoom={14}
+        >
+          <Marker
+            onClick={this.onMarkerClick}
+            icon={{
+              url: "/img/icon.svg",
+              anchor: new google.maps.Point(32, 32),
+              scaledSize: new google.maps.Size(64, 64)
+            }}
+            name={"Current location"}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyB6U8i3r63deshGQgql5RCaYy8o2qEBths",
+  v: "3"
+})(MapContainer);
