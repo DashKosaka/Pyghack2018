a179af3dbc88602e16b2a9a83178a280a7b777c4
hcwoo2 <hcwoo2@illinois.edu>
2018-09-29 16:51:52 -0500

    Removed React banner

--- google-map-pyghack2018/src/App.js
+++ google-map-pyghack2018/src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
+                {/*<header className="App-header">*/}
+                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
+                    {/*<h1 className="App-title">Welcome to React</h1>*/}
+                {/*</header>*/}
                 <Map/>
             </div>
         );
