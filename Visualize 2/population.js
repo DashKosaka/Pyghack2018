0d8d0893d8893505ec7f068e8e1d10bcd5c0bff6
dashkosaka@gmail.com <dashkosaka@gmail.com>
2018-09-30 03:34:36 -0500

    holy shyt

--- Visualize 2/population.js
+++ Visualize 2/population.js
@@ -1,18 +1,24 @@
 function populationAtLocation(data, day, time) {
 
-	populations = {}
-	var curr_location;
-	for(var building in data) {
+	var populations = {}
+	var curr_building;
+    var curr_pop;
+    var curr_location;
+	for(var crn in data) {
 	
-		if(data[building].day == day && \ // Check if the day is the same
-			data[building].begin <= time && \ // Check if time is after begin time
-			time <= data[building].end) { // Check if time is before end time
-				curr_location = data[building].center;
-				curr_pop = data[building].population;
-				if(curr_location in populations)  {
-					populations[curr_location] += curr_pop;
+        // Check if the day is the same
+        // Check if time is after begin time
+        // Check if time is before end time
+		if(data[crn].Day.indexOf(day) > -1 && data[crn]["Start time"] <= time && time <= data[crn]["End time"]) { 
+				curr_building = data[crn].Building;
+				curr_pop = data[crn].Population;
+				curr_location = data[crn].Location;
+				if(curr_building in populations)  {
+					populations[curr_building]["population"] += curr_pop;
 				} else {
-					populations[curr_location] = curr_pop;
+					populations[curr_building] = {}
+					populations[curr_building]["population"] = curr_pop;
+					populations[curr_building]["center"] = curr_location; 
 				}
 		}
 	}	
