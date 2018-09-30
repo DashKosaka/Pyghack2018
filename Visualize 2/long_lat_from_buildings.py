b35f34a2a3622b3553db2f814672d2dcd5dafe20
dashkosaka@gmail.com <dashkosaka@gmail.com>
2018-09-29 23:47:42 -0500

    Clearning

---
+++ Visualize 2/long_lat_from_buildings.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 29 23:03:56 2018
+
+@author: Dash
+"""
+import requests
+import numpy as np
+import pandas as pd
+import os
+
+def get_data(building):
+    
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?&key=AIzaSyBJUuDqdMW83obU_8PlGVwwNrVx9lxNAQM' + \
+            '&address=' + building + '%20champaign'
+            
+    try:info = requests.get(url)
+    except:print('Request Denied')
+    
+    ret = info.json()
+    
+    return ret
+
+def get_location(jsn):
+    
+    try:
+        return jsn['results']['geometry']['location']
+    except:
+        print(jsn)
+        os.exit('Uhoh....')
+        
+df = pd.read_csv('dataset.csv')
+
+locations = []
+for b in df.Building:
+    resp = get_data(b)
+    locations.append(get_location(resp))
+
+locations = np.array(locations)
+df['Location'] = locations
+
+df.to_csv('dataset_final.csv', sep=',')
+
+
+
--- data/clean_courseinfo.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 29 20:35:26 2018
-
-@author: Dash
-"""
-
-from bs4 import BeautifulSoup
-import csv
